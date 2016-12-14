d68c9e94f227024efdbf92347ae7a65d1c1604d8
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-15 07:43:38 +0800

    Implement blog service's fetch function.

--- getting-started/blog/blog.js
+++ getting-started/blog/blog.js
@@ -1,12 +1,16 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.fetchBlogs = (event, context, callback) => {
+  const blogs = retrieveBlogs();
+  const responseBody = {
+      // message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: blogs,
+      input: event,
+  };
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: JSON.stringify(responseBody),
   };
 
   callback(null, response);
@@ -14,3 +18,25 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+/**
+ * A helper for retrieving a list of posted blogs
+ */
+function retrieveBlogs(){
+  // TODO: Replace these hardcoded values by calls which retrieve the values from AWS Data Storage services (e.g. SimpleDB, DynamoDB)
+  const result = [{
+    title: 'My 1st Post',
+    categories: 'General',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },{
+    title: 'My 2nd Post',
+    categories: 'General',
+    content: 'Nam posuere eu enim id hendrerit. Donec gravida elit nec dui mattis mollis. Donec hendrerit augue vitae orci rhoncus pulvinar.'
+  },{
+    title: 'My 3rd Post',
+    categories: 'General',
+    content: 'Sed non sapien justo. Praesent non sem convallis, volutpat nulla in, volutpat justo. Aenean eget nunc malesuada, rhoncus dui nec, vehicula mauris.'
+  }];
+
+  return result;
+}
