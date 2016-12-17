b4464681182524b5d6af54ff92cd0ffe0a8aed95
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-17 09:26:35 +0800

    Tidy up blogs.js

--- getting-started/blog/blogs.js
+++ getting-started/blog/blogs.js
@@ -17,19 +17,25 @@ module.exports.fetch = (event, context, callback) => {
  * A helper for retrieving a list of posted blogs
  */
 function retrieveBlogs(){
-  // TODO: Replace these hardcoded values by calls which retrieve the values from AWS Data Storage services (e.g. SimpleDB, DynamoDB)
+  // TODO: Replace these hardcoded values by calls 
+  //       which retrieve the values from AWS Data Storage services (e.g. SimpleDB, DynamoDB)
   const result = [{
     title: 'My 1st Post',
     categories: 'General',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    content: 'Lorem ipsum dolor sit amet, \
+              consectetur adipiscing elit.'
   },{
     title: 'My 2nd Post',
     categories: 'General',
-    content: 'Nam posuere eu enim id hendrerit. Donec gravida elit nec dui mattis mollis. Donec hendrerit augue vitae orci rhoncus pulvinar.'
+    content: 'Nam posuere eu enim id hendrerit. \
+             Donec gravida elit nec dui mattis mollis. \
+             Donec hendrerit augue vitae orci rhoncus pulvinar.'
   },{
     title: 'My 3rd Post',
     categories: 'General',
-    content: 'Sed non sapien justo. Praesent non sem convallis, volutpat nulla in, volutpat justo. Aenean eget nunc malesuada, rhoncus dui nec, vehicula mauris.'
+    content: 'Sed non sapien justo. Praesent non sem convallis, \
+              volutpat nulla in, volutpat justo. \
+              Aenean eget nunc malesuada, rhoncus dui nec, vehicula mauris.'
   }];
 
   return result;
