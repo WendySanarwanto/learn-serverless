526bf99c107b4b63bb6357065f5eeb642868eba3
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-18 12:56:10 +0800

    Replaced call to retrieveBlogs with a call to BlogsRepository's retrieveBlogs method

--- getting-started/blog/blogs.js
+++ getting-started/blog/blogs.js
@@ -1,7 +1,14 @@
 'use strict';
 
+// I wish node.js 4.3.2 has supported destructuring assignment 
+// so that I could destructure contents of repositoris using this code
+// import { BlogsRepository, OtherRepositories } from './repositories';
+let BlogsRepository = require('./repositories').BlogsRepository;
+
 module.exports.fetch = (event, context, callback) => {
-  const results = retrieveBlogs();
+  let blogsRepo = new BlogsRepository();
+  const results = blogsRepo.retrieveBlogs();
+
   const response = {
     statusCode: 200,
     body: JSON.stringify(results),
@@ -12,31 +19,3 @@ module.exports.fetch = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
-
-/**
- * A helper for retrieving a list of posted blogs
- */
-function retrieveBlogs(){
-  // TODO: Replace these hardcoded values by calls 
-  //       which retrieve the values from AWS Data Storage services (e.g. SimpleDB, DynamoDB)
-  const result = [{
-    title: 'My 1st Post',
-    categories: 'General',
-    content: 'Lorem ipsum dolor sit amet, \
-              consectetur adipiscing elit.'
-  },{
-    title: 'My 2nd Post',
-    categories: 'General',
-    content: 'Nam posuere eu enim id hendrerit. \
-             Donec gravida elit nec dui mattis mollis. \
-             Donec hendrerit augue vitae orci rhoncus pulvinar.'
-  },{
-    title: 'My 3rd Post',
-    categories: 'General',
-    content: 'Sed non sapien justo. Praesent non sem convallis, \
-              volutpat nulla in, volutpat justo. \
-              Aenean eget nunc malesuada, rhoncus dui nec, vehicula mauris.'
-  }];
-
-  return result;
-}
