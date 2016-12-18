e1672c817d9173fb367c396b22aaaece8b2449ee
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-19 06:55:04 +0800

    Refactored DynamoDbDataService to retrieve records from AWS DynamoDB using AWSSDK

--- getting-started/blog/services/dynamoDb.service.js
+++ getting-started/blog/services/dynamoDb.service.js
@@ -1,9 +1,19 @@
 'use strict';
 
+let AWS = require("aws-sdk");
+
 class DynamoDbDataService {
     constructor(tableName, numberOfItems){
         this.tableName = tableName;
         this.numberOfItems = numberOfItems;
+
+        const awsDefaultRegion = process.env.AWS_DEFAULT_REGION;
+
+        let dynamoDbParams = {
+            region: awsDefaultRegion
+        };
+
+        AWS.config.update(dynamoDbParams);
     }
 
     /**
@@ -11,26 +21,20 @@ class DynamoDbDataService {
      */
     getAll(){
         return new Promise((resolve, reject)=>{
-            // TODO: Replace these hardcoded lines with calls to DynamoDb
-            const result = [{
-                title: 'My 1st Post',
-                categories: 'General',
-                content: 'Lorem ipsum dolor sit amet, \
-                        consectetur adipiscing elit.'
-            },{
-                title: 'My 2nd Post',
-                categories: 'General',
-                content: 'Nam posuere eu enim id hendrerit. \
-                        Donec gravida elit nec dui mattis mollis. \
-                        Donec hendrerit augue vitae orci rhoncus pulvinar.'
-            },{
-                title: 'My 3rd Post',
-                categories: 'General',
-                content: 'Sed non sapien justo. Praesent non sem convallis, \
-                        volutpat nulla in, volutpat justo. \
-                        Aenean eget nunc malesuada, rhoncus dui nec, vehicula mauris.'
-            }];
-            resolve(result);            
+            let docClient = new AWS.DynamoDB.DocumentClient();
+            const params = { 
+                TableName: this.tableName, 
+                Limit: this.numberOfItems 
+            };
+            
+            docClient.scan(params, (err, data) => {
+                if (err){
+                    reject(err);
+                }
+                else{
+                    resolve(data.Items);
+                }
+            });
         });
     }
 }
